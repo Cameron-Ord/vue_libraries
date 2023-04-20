7a663d29821d0541318edc7fc6e6497aee317303
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-04-20 17:37:47 -0600

    first commit

---
+++ src/components/AllUsers.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+    export default {
+        
+
+
+
+
+        mounted(){
+
+            axios({
+                method: `GET`,
+            
+                url:`https://reqres.in/api/users`
+            
+
+            })
+
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
